3ed75b4cc520745e81e9faa20d46d307b8757d60
Femon gamel <femongamel102@gmail.com>
2022-09-01 13:57:04 +0200

    handle sharedPreferences and Theme

---
+++ lib/shared/cubit/cubit.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:news_app/shared/cubit/states.dart';
+import 'package:news_app/shared/network/local/cache_helper.dart';
+
+class AppCubit extends Cubit<AppStates>{
+  AppCubit() : super(AppInitialState());
+
+  static AppCubit get(context) => BlocProvider.of(context);
+
+  bool isDark = false;
+
+  void changeAppMode({bool? fromShared}){
+    if(fromShared != null){
+      print(fromShared);
+      isDark = fromShared;
+      emit(AppChangeModeState());
+    }
+    else {
+      isDark = !isDark;
+      CacheHelper.putBoolean(key: 'isDark', value: isDark).then((value) {
+        emit(AppChangeModeState());
+      });
+    }
+
+  }
+
+}
